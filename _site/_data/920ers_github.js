ea8c5ec948db6ddf4dd36f6b9be67359cdab3a72
Craig - qrayg <craig@craigerskine.com>
2023-09-07 11:15:27 -0500

    ...

---
+++ _site/_data/920ers_github.js
@@ -0,0 +1,27 @@
+const EleventyFetch = require('@11ty/eleventy-fetch');
+
+module.exports = async function() {
+  const arr = [];
+  try {
+    let githubHandles = await EleventyFetch('https://api.github.com/repos/craigerskine/web920.com/subscribers', {
+      duration: "1d",
+      type: "json",
+    });
+    for (const githubHandle of githubHandles) {
+      const profile = await fetchGitHubUser(githubHandle.login)
+      arr.push(profile);
+    }
+    return arr;
+  } catch(e) {
+    console.log( "Failed getting GitHub users, returning 0" );
+  }
+}
+
+async function fetchGitHubUser(githubHandle) {
+  console.log(`Fetching GitHub data... ${githubHandle}`);
+  let github = await EleventyFetch(`https://api.github.com/users/${githubHandle}`, {
+    duration: "1d",
+    type: "json",
+  })
+  return github;
+};
